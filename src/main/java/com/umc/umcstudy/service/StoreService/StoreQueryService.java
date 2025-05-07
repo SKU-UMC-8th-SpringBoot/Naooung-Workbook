56ecf5b91b10f6961ae68cda4d0017c03d0ec98a
naooung <1030n@naver.com>
2025-05-07 14:21:40 +0900

    :sparkles: Feat: Service 추가

---
+++ src/main/java/com/umc/umcstudy/service/StoreService/StoreQueryService.java
@@ -0,0 +1,11 @@
+package com.umc.umcstudy.service.StoreService;
+
+import com.umc.umcstudy.domain.entity.Store;
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreQueryService {
+
+  Optional<Store> findStore(Long id);
+  List<Store> findStoresByNameAndScore(String name, Float score);
+}
