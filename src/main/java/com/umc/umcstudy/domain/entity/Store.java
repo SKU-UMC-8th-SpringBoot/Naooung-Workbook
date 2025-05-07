ed53057dd4f76420c9f2aa235a6901e04e0a366d
naooung <1030n@naver.com>
2025-05-07 14:51:03 +0900

    :sparkles: Feat: Store에 로그 출력을 위한 toString() 추가

--- src/main/java/com/umc/umcstudy/domain/entity/Store.java
+++ src/main/java/com/umc/umcstudy/domain/entity/Store.java
@@ -1,13 +1,18 @@
 package com.umc.umcstudy.domain.entity;
 
 import com.umc.umcstudy.domain.common.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,8 +38,24 @@ public class Store extends BaseEntity {
 
   private Float score;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "region_id")
   private Region region;
 
+  @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+  private List<Mission> missionList = new ArrayList<>();
+
+  @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+  private List<Review> reviewList = new ArrayList<>();
+
+  @Override
+  public String toString() {
+    return "Store{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", address='" + address + '\'' +
+        ", score=" + score +
+        ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+        '}';
+  }
 }
